d22b71eb4c5112e6c5d9e8e219d4de6572000442
Frank <frank_fan_1201@outlook.com>
2020-12-16 19:23:23 -0800

    Update percolation.py

--- percolation.py
+++ percolation.py
@@ -2,8 +2,12 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-N = 10
-P = 0.5
+N = 10  # width and length of the network
+P = 0.5  # probability of a given site to be open
 
 network = np.zeros((N, N))
+for y in range(N):
+    for x in range(N):
+        if random.random() < P:
+            network[y][x] = 1
 print(network)

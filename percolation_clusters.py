4eed9e9fb7fbd48c6ef069bac43879cfa43c945c
Frank <frank_fan_1201@outlook.com>
2020-12-17 14:52:38 -0800

    Update percolation_clusters.py

--- percolation_clusters.py
+++ percolation_clusters.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-N = 100  # width and length of the network
+N = 10  # width and length of the network
 P = 0.6  # probability of a given site to be open
 
 open = np.zeros((N, N))
@@ -11,40 +11,46 @@ for y in range(N):
     for x in range(N):
         if random.random() < P:
             open[y][x] = 1
-print(open)
+# print(open)
 
 
-def update_full(x_, y_):
+def update_full(x_, y_, id_):
     if x_ < 0 or x_ >= N:
         return None
-    elif y_ < 0 or y_ >= N:
+    if y_ < 0 or y_ >= N:
         return None
-    elif not open[y_][x_]:
+    if not open[y_][x_]:
         return None
-    elif full[y_][x_]:
+    if full[y_][x_]:
         return None
 
-    full[y_][x_] = 1
+    full[y_][x_] = id_
 
-    update_full(x_ + 1, y_)
-    update_full(x_ - 1, y_)
-    update_full(x_, y_ + 1)
-    update_full(x_, y_ - 1)
+    update_full(x_ + 1, y_, id_)
+    update_full(x_ - 1, y_, id_)
+    update_full(x_, y_ + 1, id_)
+    update_full(x_, y_ - 1, id_)
 
 
-for x in range(N):
-    update_full(x, 0)
+id = 0
+for y in range(N):
+    for x in range(N):
+        if not full[y][x]:
+            id += 1
+            update_full(x, y, id)
+print(full)
+
 
 percolates = False
 for x in range(N):
-    if full[N - 1][x] == 1:
+    if not full[N - 1][x]:
         percolates = True
 print(percolates)
 
 plt.figure()
-plt.imshow(open, cmap="gray")
+plt.imshow(open, cmap='gray')
 
 plt.figure()
-plt.imshow(full, cmap="gray")
+plt.imshow(full)
 
 plt.show()

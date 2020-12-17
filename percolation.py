44a488df9490b14c31c4fd0e4c6737c687afab64
Frank <frank_fan_1201@outlook.com>
2020-12-16 21:02:05 -0800

    Update percolation.py

--- percolation.py
+++ percolation.py
@@ -5,12 +5,46 @@ import matplotlib.pyplot as plt
 N = 10  # width and length of the network
 P = 0.5  # probability of a given site to be open
 
-network = np.zeros((N, N))
+open = np.zeros((N, N))
+full = np.zeros((N, N))
 for y in range(N):
     for x in range(N):
         if random.random() < P:
-            network[y][x] = 1
-print(network)
+            open[y][x] = 1
+print(open)
 
-plt.imshow(network, cmap="gray")
+
+def update_full(x_, y_, open_, full_):
+    if x_ < 0 or x_ >= N:
+        return None
+    elif y_ < 0 or y_ >= N:
+        return None
+    elif not open_[y_][x_]:
+        return None
+    elif full_[y_][x_]:
+        return None
+
+    full_[y_][x_] = 1
+
+    update_full(x_ + 1, y_, open_, full_)
+    update_full(x_ - 1, y_, open_, full_)
+    update_full(x_, y_ + 1, open_, full_)
+    update_full(x_, y_ - 1, open_, full_)
+
+
+for x in range(N):
+    update_full(x, 0, open, full)
+
+percolates = False
+for x in range(N):
+    if full[N - 1][x] == 1:
+        percolates = True
+print(percolates)
+
+plt.figure()
+plt.imshow(open, cmap="gray")
+
+plt.figure()
+plt.imshow(full, cmap="gray")
+
 plt.show()

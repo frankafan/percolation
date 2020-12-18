c206a0c0ef361e9c0c6371064c9ec0b542811420
Frank <frank_fan_1201@outlook.com>
2020-12-18 14:08:22 -0800

    Update bond_percolation2.py

--- bond_percolation2.py
+++ bond_percolation2.py
@@ -3,33 +3,26 @@ import random
 import matplotlib.pyplot as plt
 
 P = 0.6
-N = 100
+N = 10
 
 lattice = np.zeros((N, N))
-vertical_bonds = []
-horizontal_bonds = []
+bonds = []
 for y in range(N):
     for x in range(N):
         if y < N - 1:
-            vertical_bonds.append([[x, y], [x, y + 1]])
+            bonds.append([[x, y], [x, y + 1]])
         if x < N - 1:
-            horizontal_bonds.append([[x, y], [x + 1, y]])
+            bonds.append([[x, y], [x + 1, y]])
 
-for i in range(len(vertical_bonds)):
+for i in range(len(bonds)):
     if random.random() < P:
-        vertical_bonds[i] = False
-    if random.random() < P:
-        horizontal_bonds[i] = False
+        bonds[i] = False
 
 
-for i in range(len(vertical_bonds)):
-    if vertical_bonds[i]:
-        vertical_bond_x = [vertical_bonds[i][0][0], vertical_bonds[i][1][0]]
-        vertical_bond_y = [vertical_bonds[i][0][1], vertical_bonds[i][1][1]]
-        plt.plot(vertical_bond_x, vertical_bond_y, 'black')
-    if horizontal_bonds[i]:
-        horizontal_bond_x = [horizontal_bonds[i][0][0], horizontal_bonds[i][1][0]]
-        horizontal_bond_y = [horizontal_bonds[i][0][1], horizontal_bonds[i][1][1]]
-        plt.plot(horizontal_bond_x, horizontal_bond_y, 'black')
+for i in range(len(bonds)):
+    if bonds[i]:
+        bond_x = [bonds[i][0][0], bonds[i][1][0]]
+        bond_y = [bonds[i][0][1], bonds[i][1][1]]
+        plt.plot(bond_x, bond_y, 'black')
 
 plt.show()

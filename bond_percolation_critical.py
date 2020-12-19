b9ca87c89fa54a4fa2b30e40926f38d4a1e9425f
Frank <frank_fan_1201@outlook.com>
2020-12-18 16:01:25 -0800

    Update bond_percolation_critical.py

--- bond_percolation_critical.py
+++ bond_percolation_critical.py
@@ -2,8 +2,9 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-P = 0.49
-N = 50
+N = 25
+trials = 1
+SAVEFIG = False
 
 
 def update_lattice(x_, y_):
@@ -24,49 +25,55 @@ def update_lattice(x_, y_):
             update_lattice(bond[0][0], bond[0][1])
 
 
-bonds = []
-for y in range(N):
-    for x in range(N):
-        if y < N - 1:
-            bonds.append([[x, y], [x, y + 1]])
-        if x < N - 1:
-            bonds.append([[x, y], [x + 1, y]])
+percolation_rates = []
+j = 1
+for P in np.arange(0.1, 0.7, 0.01):
+    print(P)
+    percolations = 0
 
-for i in range(len(bonds)):
-    if random.random() < (1 - P):
-        bonds[i] = False
-# print(bonds)
+    for n in range(trials):
+        bonds = []
+        for y in range(N):
+            for x in range(N):
+                if y < N - 1:
+                    bonds.append([[x, y], [x, y + 1]])
+                if x < N - 1:
+                    bonds.append([[x, y], [x + 1, y]])
 
-bond_flattened = []
-for bond in bonds:
-    if bond:
-        bond_flattened.extend(bond)
-# print(bond_flattened)
+        for i in range(len(bonds)):
+            if random.random() < (1 - P):
+                bonds[i] = False
+        # print(bonds)
+
+        bond_flattened = []
+        for bond in bonds:
+            if bond:
+                bond_flattened.extend(bond)
+        # print(bond_flattened)
+
+        lattice = np.zeros((N, N))
+
+        for x in range(N):
+            update_lattice(x, 0)
+
+        percolates = False
+        for x in range(N):
+            if lattice[N - 1][x] == 1:
+                percolates = True
+        # print(percolates)
+
+        if percolates:
+            percolations += 1
+
+    percolation_rates.append(percolations / trials)
 
 plt.figure()
-for i in range(len(bonds)):
-    if bonds[i]:
-        bond_x = []
-        bond_y = []
-        for j in range(len(bonds[i])):
-            bond_x.append(bonds[i][j][0])
-            bond_y.append(bonds[i][j][1])
-        plt.plot(bond_x, bond_y, 'black')
-plt.ylim([N, 0])
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title(f"Lattice of connected bonds at p = {round(P, 2)}")
-
-lattice = np.zeros((N, N))
-
-for x in range(N):
-    update_lattice(x, 0)
-
-plt.figure()
-plt.imshow(lattice, cmap='gray')
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title(
-    f"Lattice of spaces connected to the top edge by bonds at p = {round(P, 2)}")
+plt.plot(np.arange(0.1, 0.7, 0.01), percolation_rates)
+plt.xlabel("$p$")
+plt.ylabel("Percolation probability")
+plt.title(f"Percolation probability over $p$ for {trials} trials")
+if SAVEFIG:
+    plt.savefig(f"Percolation probability over p for {trials} trials.png")
+plt.show()
 
 plt.show()
